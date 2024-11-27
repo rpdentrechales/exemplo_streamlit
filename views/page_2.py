79779aabdd92818347ea67b58a693aee182ca518
rpdentrechales <rpdprocorpo@gmail.com>
2024-11-27 16:25:54 +0000

    Updating files

--- views/page_2.py
+++ views/page_2.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from auxiliar.aux_vendas import *
 
-df = gerar_dados_fakes()
-st.title("Teste Streamlit - Página 2")
 
-st.dataframe(df)
+st.title("Teste Streamlit - Página 2")
+df = gerar_dados_fakes()
+
+
+
+st.dataframe(df,use_container_width= True)
