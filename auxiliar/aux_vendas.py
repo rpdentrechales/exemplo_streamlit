45a66277a6ebf63a470825d94350ad2199bfdd67
rpdentrechales <rpdprocorpo@gmail.com>
2024-11-27 16:21:40 +0000

    Updating files

---
+++ auxiliar/aux_vendas.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+
+def gerar_dados_fakes():
+  # Generate a sample DataFrame
+  np.random.seed(42)
+  rows = 100  # Number of rows in the DataFrame
+
+  data = {
+      "Date": pd.date_range(start="2023-01-01", periods=rows, freq="D"),
+      "Sales": np.random.randint(100, 1000, size=rows),
+      "Category": [random.choice(["Electronics", "Clothing", "Home", "Books", "Toys"]) for _ in range(rows)],
+      "Region": [random.choice(["North", "South", "East", "West"]) for _ in range(rows)],
+      "Customer_Type": [random.choice(["New", "Returning"]) for _ in range(rows)],
+  }
+
+  df = pd.DataFrame(data)
+  return df
--- views/page_2.py
+++ views/page_2.py
@@ -1,3 +1,7 @@
 import streamlit as st
+from auxiliar.aux_vendas import *
 
+df = gerar_dados_fakes()
 st.title("Teste Streamlit - Página 2")
+
+st.dataframe(df)
