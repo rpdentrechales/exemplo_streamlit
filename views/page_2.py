8b6aa3c78a8d77e86f19d9988ce20d45eadf4e10
rpdentrechales <rpdprocorpo@gmail.com>
2024-11-27 16:27:33 +0000

    Updating files

--- views/page_2.py
+++ views/page_2.py
@@ -5,6 +5,8 @@ from auxiliar.aux_vendas import *
 st.title("Teste Streamlit - Página 2")
 df = gerar_dados_fakes()
 
+seletor_mes = st.selectbox("Selecione um mês", df['Month'].unique())
 
+filtered_df = df.loc[df['Month'] == seletor_mes]
 
-st.dataframe(df,use_container_width= True)
+st.dataframe(filtered_df,use_container_width= True)
